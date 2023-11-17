51ac4ed664da00fa59524da40ed0ddaf0a14644b
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-11-17 16:03:27 +0100

    move logs in folder

--- R/slurm.R
+++ R/slurm.R
@@ -3,12 +3,13 @@ startComp <- function(cmdStr, prefix="DEEB") {
   if (isSlurmAvailable()) {
     jobName <- paste0(prefix, "_", format(Sys.time(), "%Y-%m-%d_%H-%M-%S"))
     cat("Starting SLURM job", jobName, "\n")
+    if (!dir.exists("_log")) dir.create("_log")
     clcom <- paste0(
       "sbatch ",
       " --qos=short",
       " --job-name=", jobName,
-      " --output=", jobName, "_%j.out",
-      " --error=", jobName, "_%j.err",
+      " --output=_log/", jobName, "_%j.out",
+      " --error=_log/", jobName, "_%j.err",
       " --mail-type=END",
       " --wrap=\"Rscript -e '", gsub("\"", "\\\\\"", cmdStr), "'\"")
     cat(clcom, "\n")
