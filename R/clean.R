57480a934053351b854e103d2fc9eeb35d56fc53
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-04-12 15:13:15 +0200

    more verbose when cleaning

--- R/clean.R
+++ R/clean.R
@@ -1,6 +1,11 @@
 cleanLogDir <- function(dbPath) {
   logDir <- DEEBpath::getLogDir(dbPath, relative=FALSE)
   if (dir.exists(logDir)) {
+    nFiles <- length(list.files(logDir))
+    cat("Deleting", nFiles, "files in", logDir, "\n")
     unlink(logDir, recursive = TRUE, force = TRUE)
+    cat("Done.\n")
+    return(invisible())
   }
+  cat("No log files found.\n")
 }
